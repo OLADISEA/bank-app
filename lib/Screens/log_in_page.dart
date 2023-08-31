f46a9381934c5a86d16026e5af2f497f42735a98
OLADISEA <ladisea55@gmail.com>
2023-08-31 12:38:48 +0100

    Added the password visibility, refactored some navigation and made some changes in the UI

--- lib/Screens/log_in_page.dart
+++ lib/Screens/log_in_page.dart
@@ -1,10 +1,10 @@
 import 'package:bank_app/Screens/sign_up.dart';
 import 'package:bank_app/Utilities/colors.dart';
+import 'package:bank_app/Utilities/dimensions.dart';
 import 'package:email_validator/email_validator.dart';
-import 'package:flutter/gestures.dart';
+//import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '';
 import '../Utilities/card.dart';
 
 class LoginPage extends StatefulWidget {
@@ -19,6 +19,7 @@ class _LoginPageState extends State<LoginPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool hidePassword = true;
 
 
 
@@ -54,11 +55,17 @@ class _LoginPageState extends State<LoginPage> {
 
       if (userId != null) {
         // Successful sign-in
-        Navigator.pushNamed(context, '/home', arguments: userId);
+        Navigator.pushNamedAndRemoveUntil(
+          context,
+          '/home',
+              (Route<dynamic> route) => false,
+          arguments: userId,
+        );
+
       } else {
         // Invalid credentials
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Invalid credentials')),
+          const SnackBar(content: Text('Invalid credentials')),
         );
       }
     }
@@ -69,183 +76,180 @@ class _LoginPageState extends State<LoginPage> {
     double w = MediaQuery.of(context).size.width;
     double h = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              width: w,
-              height: h * 0.3,
-              decoration: const BoxDecoration(
-                  color: AppColors.mainColor,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 20, right: 20),
-              width: w,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+    return Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    margin: const EdgeInsets.only(top: 25),
-                    child: Text(
-                      "Sign in",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey[500],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 50),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30),
-                      boxShadow: [
-                        BoxShadow(
-                          spreadRadius: 7,
-                          blurRadius: 10,
-                          offset: const Offset(1, 1),
-                          color: Colors.grey.withOpacity(0.2),
-                        ),
-                      ],
-                    ),
-                    child: TextFormField(
-                      validator: (value) {
-                        if (value == "") {
-                          return "Email cannot be empty";
-                        } else if (!EmailValidator.validate(value!)) {
-                          return "Invalid Email";
-                        }
-                      },
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        hintText: "Email",
-                        prefixIcon: const Icon(
-                          Icons.email,
-                          color: Colors.deepOrangeAccent,
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: const BorderSide(
-                            color: Colors.white,
-                            width: 1.0,
-                          ),
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30),
-                      boxShadow: [
-                        BoxShadow(
-                          spreadRadius: 7,
-                          blurRadius: 10,
-                          offset: const Offset(1, 1),
-                          color: Colors.grey.withOpacity(0.2),
-                        ),
-                      ],
-                    ),
-                    child: TextFormField(
-                      validator: (value) {
-                        if (value == "") {
-                          return "Password cannot be empty";
-                        } else if (value!.length < 6) {
-                          return 'Password must be at least 6 characters';
-                        }
-                      },
-                      obscureText: true,
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                        hintText: "Password",
-                        prefixIcon: const Icon(
-                          Icons.password_outlined,
-                          color: Colors.deepOrangeAccent,
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: const BorderSide(
-                            color: Colors.white,
-                            width: 1.0,
-                          ),
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(height: 40,),
-            Container(
-              width: w * 0.8,
-              height: h * 0.05,
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    login();
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                ),
-                child: const Text(
-                  "Sign in",
+                  Text("Sign in",
                   style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: w * 0.11),
-            RichText(
-              text: TextSpan(
-                text: "Don't have an account? ",
-                style: TextStyle(
-                  color: Colors.grey[500],
-                  fontSize: 20,
-                ),
-                children: [
-                  TextSpan(
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () => Get.to(() => SignUp()),
-                    text: "Create",
-                    style: TextStyle(
-                      color: Colors.grey[500],
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                    fontSize: 23,
+                    color: Colors.white
+                  ),),
+                  Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(15)
+                      ),
+                      child: Icon(Icons.arrow_downward))
                 ],
               ),
-            ),
-          ],
+              Container(
+                margin: const EdgeInsets.only(left: 1,right: 1,top: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  //borderRadius: BorderRadius.circular(0),
+                  boxShadow: [
+                    BoxShadow(
+                      spreadRadius: 7,
+                      blurRadius: 10,
+                      offset: const Offset(1, 1),
+                      color: Colors.grey.withOpacity(0.2),
+                    ),
+                  ],
+                ),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == "") {
+                      return "Email cannot be empty";
+                    } else if (!EmailValidator.validate(value!)) {
+                      return "Invalid Email";
+                    }
+                    return null;
+                  },
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    hintText: "Email",
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Colors.deepOrangeAccent,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      //borderRadius: BorderRadius.circular(Dimensions.radius30),
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.0,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder( // Add a white glow when focused
+                      //borderRadius: BorderRadius.circular(Dimensions.radius30),
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 2.0,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                     // borderRadius: BorderRadius.circular(Dimensions.radius30),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: Dimensions.height20),
+              Container(
+                margin: const EdgeInsets.only(left: 1,right: 1,top: 5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                 // borderRadius: BorderRadius.circular(Dimensions.radius30),
+                  boxShadow: [
+                    BoxShadow(
+                      spreadRadius: 7,
+                      blurRadius: 10,
+                      offset: const Offset(1, 1),
+                      color: Colors.white.withOpacity(0.2),
+                    ),
+                  ],
+                ),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == "") {
+                      return "Password cannot be empty";
+                    } else if (value!.length < 6) {
+                      return 'Password must be at least 6 characters';
+                    }
+                    return null;
+                  },
+                  obscureText: hidePassword,
+                  controller: _passwordController,
+                  style: const TextStyle(
+                    color: Colors.black
+                  ),
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                        onPressed: (){
+                          setState(() {
+                            hidePassword = !hidePassword;
+                          });
+                        }, icon: Icon(hidePassword?Icons.visibility_off:Icons.visibility)),
+                    hintText: "Password",
+                    prefixIcon: Icon(
+                      Icons.lock_clock_rounded,
+                      color: Colors.deepOrangeAccent,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      //borderRadius: BorderRadius.circular(Dimensions.radius30),
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.0,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder( // Add a white glow when focused
+                      //borderRadius: BorderRadius.circular(Dimensions.radius30),
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 2.0,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      //borderRadius: BorderRadius.circular(Dimensions.radius30),
+                    ),
+                  ),
+                ),
+              ),
+        const SizedBox(height: 3,),
+        Padding(
+          padding: const EdgeInsets.only(left: 250),
+          child: TextButton(
+              onPressed: (){},
+              child: Text("Forgot password?",
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.white
+              ),)
+          ),
         ),
-      ),
+        GestureDetector(
+          onTap:  () {
+            if (_formKey.currentState!.validate()) {
+              login();
+            }
+          },
+
+          child: Container(
+            margin: EdgeInsets.only(left: 20,top: Dimensions.height10,right: 20),
+
+            padding: EdgeInsets.only(left: 120,top: Dimensions.height10),
+            width: w * 0.8,
+            height: h * 0.05,
+            //color: Colors.red[600],
+            decoration: BoxDecoration(
+                 color: Colors.red[600],
+                 borderRadius: BorderRadius.all(Radius.circular(5))
+            ),
+            child: Text(
+              "Sign in",
+              style: TextStyle(
+                fontSize: Dimensions.height30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white
+              ),
+            ),
+          )),
+      ],
+    ),
     );
   }
 }
