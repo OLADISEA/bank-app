c807d7db237ce222ec4bda093c54a88827722909
OLADISEA <ladisea55@gmail.com>
2023-09-16 22:19:44 +0100

    Refactored the loading page

--- lib/Screens/loading.dart
+++ lib/Screens/loading.dart
@@ -1,10 +1,14 @@
 import 'dart:async';
+import 'package:bank_app/Screens/home_view.dart';
+import 'package:bank_app/Utilities/dimensions.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 
 import 'log_in_page.dart';
 
 class Loadpage extends StatefulWidget {
+  const Loadpage({super.key});
+
   @override
   State<Loadpage> createState() => _MyHomePageState();
 }
@@ -16,7 +20,7 @@ class _MyHomePageState extends State<Loadpage> {
     super.initState();
     Timer(const Duration(milliseconds: 1000), () {
       Navigator
-          .pushReplacement(context,PageTransition(type: PageTransitionType.fade,child: const LoginPage()));
+          .pushReplacement(context,PageTransition(type: PageTransitionType.fade,child: const HomeView()));
     });
   }
 
@@ -28,10 +32,10 @@ class _MyHomePageState extends State<Loadpage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SizedBox(
-              width: 60,
-              height: 60,
-              child: CircularProgressIndicator(
+            SizedBox(
+              width: Dimensions.height60,
+              height: Dimensions.height60,
+              child: const CircularProgressIndicator(
                 strokeWidth: 4,
                 color: Color.fromARGB(255, 255, 0, 191),
               ),
