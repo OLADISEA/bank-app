9f06cffe9990507af6e17f9f96cde62914c9d7d6
OLADISEA <ladisea55@gmail.com>
2023-09-16 22:22:40 +0100

    Include the Home view screen size

--- lib/Utilities/dimensions.dart
+++ lib/Utilities/dimensions.dart
@@ -6,6 +6,7 @@ class Dimensions{
   static double screenHeight = Get.context!.height;
   static double screenWidth = Get.context!.width;
 
+  static double pageViewHome = screenHeight/3.653;//300
   static double pageView = screenHeight/3.425;//320
   static double sized = screenHeight/36.533;//30
   static double small = screenHeight/109.6;//10
@@ -21,6 +22,12 @@ class Dimensions{
   static double height45 = screenHeight/24.356;//45
   static double height30 = screenHeight/36.533;//30
   static double height10 = screenHeight/109.6;//10
+  static double height16 = screenHeight/68.5;//16
+  static double height80 = screenHeight/13.7;//80
+  static double height60 = screenHeight/18.267;//60
+
+
+
   static double height100 = screenHeight/10.96;//100
 
 
@@ -45,6 +52,12 @@ class Dimensions{
 
   static double firstContainer = screenHeight/4.882;//220
   static double pageView140 = screenHeight/7.82857;//140
+  static double elevated120 = screenHeight/9.1333;//120
+  static double elevated180 = screenHeight/6.089;//180
+  static double elevated250 = screenHeight/4.384;//250
+
+
+
   static double font20 = screenHeight/54.8;
   static double font12 = screenHeight/91.33;
 
