e85b006918eeed5ac19a70f0f8a830a805d269f3
OLADISEA <ladisea55@gmail.com>
2023-09-16 22:19:02 +0100

    Made adjustment to the transaction.dart file

--- lib/Utilities/transaction.dart
+++ lib/Utilities/transaction.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+enum TransactionType {
+  receive,
+  send,
+}
+
 class Transaction {
   final String description;
   final String date;
@@ -7,18 +12,63 @@ class Transaction {
   final String senderName;
   final String receiverId;
   final String senderId;
+  final TransactionType? type;
 
-
-  Transaction({required this.senderId,required this.receiverId,required this.senderName, this.description = 'Transfer', required this.date, required this.amount});
+  Transaction({
+    required this.senderId,
+    required this.receiverId,
+    required this.senderName,
+    this.description = 'Transfer',
+    required this.date,
+    required this.amount,
+    required this.type,
+  });
 }
+
 List<Transaction> transactions = [
-  Transaction(senderId: '',receiverId: '',senderName:'Tosin',description: 'School Fees', date: '7 jun 2023', amount: 5000.00),
-  Transaction(senderId: '',receiverId: '',senderName:'Olamide',description: 'Vehicle purchase', date: '6 july,2023', amount: 34500.00),
-  Transaction(senderId: '',receiverId: '',senderName:'Isaac',description: 'Lesson', date: '7 jun,2023', amount: 1200.00),
-  Transaction(senderId: '',receiverId: '',senderName:'Emmanuel',description: 'Vehicle purchase', date: '6 july,2023', amount: 34500.00),
-  Transaction(senderId: '',receiverId: '',senderName:'Ezekiel',description: 'Vehicle purchase', date: '6 july,2023', amount: 34500.00),
-
-
-
-
+  Transaction(
+    senderId: '',
+    receiverId: '',
+    senderName: 'Tosin',
+    description: 'School Fees',
+    date: '7 jun 2023',
+    amount: 5000.00,
+    type: TransactionType.send,
+  ),
+  Transaction(
+    senderId: '',
+    receiverId: '',
+    senderName: 'Olamide',
+    description: 'Vehicle purchase',
+    date: '6 july,2023',
+    amount: 34500.00,
+    type: TransactionType.receive,
+  ),
+  Transaction(
+    senderId: '',
+    receiverId: '',
+    senderName: 'Isaac',
+    description: 'Lesson',
+    date: '7 jun,2023',
+    amount: 1200.00,
+    type: TransactionType.receive,
+  ),
+  Transaction(
+    senderId: '',
+    receiverId: '',
+    senderName: 'Emmanuel',
+    description: 'Vehicle purchase',
+    date: '6 july,2023',
+    amount: 34500.00,
+    type: TransactionType.send,
+  ),
+  Transaction(
+    senderId: '',
+    receiverId: '',
+    senderName: 'Ezekiel',
+    description: 'Vehicle purchase',
+    date: '6 july,2023',
+    amount: 34500.00,
+    type: TransactionType.send,
+  ),
 ];
